3df17e0e6b71734491f5e29fe3d32b8a0762bc5c
Tomas <tomasvareika85@gmail.com>
2023-07-20 17:48:46 +0300

    IsPublished migrations

--- back/TV-IDP/TV-IDP/Access/Model/Post.cs
+++ back/TV-IDP/TV-IDP/Access/Model/Post.cs
@@ -7,5 +7,7 @@
         public required string Title { get; set; }
 
         public required string Body { get; set; }
+
+        public bool IsPublished { get; set; }
     }
 }
---
+++ back/TV-IDP/TV-IDP/Migrations/20230720144424_AddIsPublishedToPost.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TV_IDP.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsPublishedToPost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPublished",
+                table: "Posts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPublished",
+                table: "Posts");
+        }
+    }
+}
