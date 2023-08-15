171f1702fe600a0cb80cbeb699fe201bb748ab99
Tomas <tomasvareika85@gmail.com>
2023-08-15 17:25:12 +0300

    migration warnings

--- back/Migrations/20230720083357_initial.Designer.cs
+++ back/Migrations/20230720083357_initial.Designer.cs
@@ -12,7 +12,7 @@ namespace TV_IDP.Migrations
 {
     [DbContext(typeof(AppDbContext))]
     [Migration("20230720083357_initial")]
-    partial class initial
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
