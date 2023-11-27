32ecb4f821b3ffc233ffb511fd04d980c260c548
Tomas <tomasvareika85@gmail.com>
2023-11-27 14:45:10 +0200

    chess controller

--- back/Controllers/ChessController.cs
+++ back/Controllers/ChessController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using TV_IDP.Access.Models;
 using TV_IDP.Authorization;
-using TV_IDP.Services;
+using TV_IDP.Handlers.Chess;
 
 namespace TV_IDP.Controllers;
 
@@ -11,38 +9,10 @@ namespace TV_IDP.Controllers;
 [Route("api/[controller]")]
 public class ChessController : ControllerBase
 {
-    private readonly AppDbContext _db;
-
-    public ChessController(AppDbContext db)
-    {
-        _db = db;
-    }
-
     [HttpGet(nameof(GetChessGameDetails))]
     public async Task<IActionResult> GetChessGameDetails(int gameId)
     {
-        var chessGame = await _db.ChessGames.FirstOrDefaultAsync(x => x.Id == gameId);
-
-        if (chessGame == null)
-        {
-            return BadRequest(new { message = "Bad request" });
-        }
-
-        var currentUser = HttpContext.Items["User"] as User;
-
-        if (currentUser == null)
-        {
-            return BadRequest(new { message = "Bad request" });
-        }
-
-        var color = chessGame.WhiteUserId == currentUser.Id ? "white" : "black";
-
-        var response = new
-        {
-            chessGame.Id,
-            color,
-        };
-
-        return Ok(response);
+        var handler = new GetChessGameDetailsRequestHandler(HttpContext);
+        return await handler.HandleAsync(gameId);
     }
 }
